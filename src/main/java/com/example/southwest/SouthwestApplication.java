6ded5be15eedf6492be855d53f92912c2abd4567
Yoni Doron-Peters <yoni.doron-peters@hybris.com>
2017-10-01 17:41:54 -0600

    Initialize new repo with some southwest login notes

---
+++ src/main/java/com/example/southwest/SouthwestApplication.java
@@ -0,0 +1,12 @@
+package com.example.southwest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SouthwestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SouthwestApplication.class, args);
+	}
+}
